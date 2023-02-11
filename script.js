e658542ecb2c96dccd0cba4faf2750b42581e03e
wwwbijay <link2bj@gmail.com>
2023-02-11 22:51:57 +0545

    initial

---
+++ script.js
@@ -0,0 +1,17 @@
+//canvas setup
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+canvas.width = 800;
+canvas.height = 500;
+
+let score = 0;
+let gameFrame = 0;
+ctx.font = "50px Georgia";
+
+//Mouse Interactivity
+
+//Player
+
+//Bubbles
+
+//Animation Loop
